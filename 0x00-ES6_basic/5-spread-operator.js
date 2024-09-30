f06fb1cc89be4112737381f4ca2b70473f090034
ayoubleass <ayoubleas@gmail.com>
2024-09-30 16:30:09 +0000

    Update task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-export default function concatArrays(arr1, arr2, str) {
-   return [...arr1, ...arr2, ...str];
+export default function concatArrays(array1, array2, string) {
+   return [...array1, ...array2, ...string];
 }  
