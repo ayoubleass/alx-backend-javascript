fcf8af94db6358a7eed333619776b8194f84dd42
ayoubleass <ayoubleas@gmail.com>
2024-09-30 14:13:53 +0000

    Update File: 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,12 @@
 export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
+  
   var task = false;
   var task2 = true;
   
