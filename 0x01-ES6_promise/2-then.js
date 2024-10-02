c77c69a5ba8dbac2ec97d27836d1a0d1a6466349
ayoubleass <ayoubleas@gmail.com>
2024-10-02 14:53:05 +0000

    create : 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-	.then(() => {
-	  return {
-	    status: 200,
-	    body: 'success',
-	  };
-	})
-        .catch(() => new Error())
-	.finally(() => console.log('Got a response from the API'));
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
