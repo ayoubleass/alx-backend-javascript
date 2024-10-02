95caa5b46b415bf2eb4e5a6c8f60bf56fe9649c2
ayoubleass <ayoubleas@gmail.com>
2024-10-02 16:21:06 +0000

    Create : file 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName ,lastName, fileName ) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map(element => ({
+        status: element.status,
+        value : element.status == 'fulfilled' ? element.value : element.reason
+      }))
+   ));
+}
