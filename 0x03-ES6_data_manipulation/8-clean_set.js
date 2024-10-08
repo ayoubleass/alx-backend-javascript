2d63a0ec4c9c7cabc32edf2a4edc524cef3275b3
ayoubleass <ayoubleas@gmail.com>
2024-10-08 15:34:27 +0000

    Update file 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!set || !(set instanceof Set) || typeof startString !== 'string' || startString === '') {
+  if (!set && !(set instanceof Set) &&typeof startString !== 'string') {
     return '';
   }
   const result = [];
   for (const item of set) {
-    if (item.startsWith(startString)) {
+    if (typeof item === 'string' && item.startsWith(startString) && startString.length > 0) {
       result.push(item.slice(startString.length));
     }
   }
