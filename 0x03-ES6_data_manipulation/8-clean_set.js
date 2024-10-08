15e33c1e3dc40c330738b60a75317659319b1709
ayoubleass <ayoubleas@gmail.com>
2024-10-08 15:39:43 +0000

    Update file 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!set && !(set instanceof Set) &&typeof startString !== 'string') {
+  if (!set && !(set instanceof Set) && typeof startString !== 'string') {
     return '';
   }
   const result = [];
