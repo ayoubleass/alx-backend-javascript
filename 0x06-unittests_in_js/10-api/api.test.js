6a9b10f1d0b455e08fa5e35d25a1c584ebadedea
ayoubleass <ayoubleas@gmail.com>
2024-11-28 14:20:20 +0000

    Createing folder 8-api 9-api and 10-api

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,37 @@
+const request = require('request');
+const { expect } = require('chai');
+
+
+describe('Testing api response', () => {
+  it('testing the request status and if the result is correct', (done) => {
+   const res = request.get('http://localhost:7865', (err, res, body) => {
+     expect(res.statusCode).to.be.equal(200);
+     expect(body).to.be.equal('Welcome to the payment system');
+     done();
+   });
+  });
+
+  it('testing /card end point', (done) => {
+    const res = request.get('http://localhost:7865/cart/1', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 1');
+      done();
+    });
+  });
+
+  it('testing /available_payments end point', (done) => {
+    const res = request.get('http://localhost:7865/available_payments', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(JSON.parse(body)).to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      done();
+    });
+  });
+
+  it('testing /login end point', (done) => {
+    const res = request.post('http://localhost:7865/login', {json: {userName: 'jhon'}}, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome jhon');
+      done();
+    });
+  });
+});
